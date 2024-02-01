56b48059579a3f6a352ad5e5f9ce56d553288b61
praviny7278 <praviny408@gmail.com>
2024-02-01 19:46:08 +0530

    persnol_portfoliyo

---
+++ script/script.js
@@ -0,0 +1,54 @@
+
+// ========== Project 1 =============//
+document.getElementById('pr1-live-link-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://first-mousse-a5130d.netlify.app/'
+});
+
+document.getElementById('pr1-git-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://github.com/RahulRathod5181/FirstCry'
+});
+
+
+
+
+// ========== Project 2 =============//
+document.getElementById('pr2-live-link-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://luminous-granita-716e69.netlify.app/'
+});
+
+document.getElementById('pr2-git-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://github.com/yash-levi1896/ceaseless-caption-4072'
+});
+
+
+
+
+
+// ========== Project 3 =============//
+document.getElementById('pr3-live-link-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://65ba699f493702ef27545d24--warm-brioche-54d8ee.netlify.app/'
+});
+
+document.getElementById('pr3-git-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://github.com/praviny7278/TODO_user_App'
+});
+
+
+
+
+// ========== Project 4 =============//
+document.getElementById('pr4-live-link-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://65ba6b4d2c9f7df2bc30341e--famous-licorice-379166.netlify.app/'
+});
+
+document.getElementById('pr4-git-btn').addEventListener('click', ()=> {
+
+    window.location.href = 'https://github.com/praviny7278/game_dev_webClone'
+});
